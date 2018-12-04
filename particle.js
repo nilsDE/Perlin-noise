ee9ba068b0ffdc48d5d1e707a0307b17e2431650
Nils <39887052+nilsDE@users.noreply.github.com>
2018-12-04 23:07:05 +0100

    added sliders and separated files

---
+++ particle.js
@@ -0,0 +1,26 @@
+class Particle {
+  constructor() {
+    this.x = floor(n);
+    this.y = floor(ny);
+    this.history = [];
+  }
+  show() {
+    strokeWeight(5);
+    point(this.x, this.y);
+    for (let i = 0; i < this.history.length; i++) {
+      let pos = this.history[i];
+      stroke(255, i);
+      strokeWeight(map(i, 0, 1000, 1, 6));
+      point(pos.x, pos.y);
+    }
+  }
+  update() {
+    this.x = n;
+    this.y = ny;
+    let v = createVector(this.x, this.y);
+    this.history.push(v);
+    if (this.history.length >= 1000) {
+      this.history.splice(0, 1);
+    }
+  }
+}
--- sketch.js
+++ sketch.js
@@ -3,19 +3,28 @@ let yoff = 500;
 let n = 200;
 let ny = 0;
 let particle;
+let incrementX = 0.005;
+let incrementY = 0.005;
+let incrSliderX, incrSliderY;
 
 function setup() {
   createCanvas(600, 600);
   pixelDensity(1);
   particle = new Particle();
+  incrSliderX = createSlider(0.001, 0.05, 0.005, 0.001);
+  incrSliderX.position(width + 10, 30);
+  incrSliderX.style('width', '160px');
+  incrSliderY = createSlider(0.001, 0.05, 0.005, 0.001);
+  incrSliderY.position(width + 10, 90);
+  incrSliderY.style('width', '160px');
+
 }
 
 function draw() {
   background(121);
-
-  xoff = xoff + 0.005;
+  xoff = xoff + incrSliderX.value();
   n = noise(xoff) * width;
-  yoff = yoff + 0.005;
+  yoff = yoff + incrSliderY.value();
   ny = noise(yoff) * height;
 
   loadPixels();
@@ -34,40 +43,9 @@ function draw() {
   updatePixels();
 
   strokeWeight(1);
-  stroke(0,120);
-  noFill();
-  rectMode(CENTER);
-  rect(width / 2, height / 2, width * 0.6, height * 0.6);
-	stroke(0);
+  stroke(0);
   line(n, 0, n, height);
   line(0, ny, width, ny);
   particle.update();
   particle.show();
-}
-
-class Particle {
-  constructor() {
-    this.x = floor(n);
-    this.y = floor(ny);
-    this.history = [];
-  }
-  show() {
-    strokeWeight(5);
-    point(this.x, this.y);
-    for (let i = 0; i < this.history.length; i++) {
-      let pos = this.history[i];
-      stroke(255, i);
-      strokeWeight(map(i, 0, 1000, 1, 6));
-      point(pos.x, pos.y);
-    }
-  }
-  update() {
-    this.x = n;
-    this.y = ny;
-    let v = createVector(this.x, this.y);
-    this.history.push(v);
-    if (this.history.length >= 1000) {
-      this.history.splice(0, 1);
-    }
-  }
 }
